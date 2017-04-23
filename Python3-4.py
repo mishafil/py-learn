0ed5c51c14aa84ffbc43f3fa85fc09f9e8b658a9
Danny <liquidcore7@gmail.com>
2017-04-23 22:47:53 +0300

    make this look like a module

--- Python3-4.py
+++ Python3-4.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-num = int(input("Write a number"))
-cnt=0
-for x in range (1, num/2):
-    if num % x == 0:
-        cnt += 1
-return cnt + 1        
+
+def divisors(num) :
+    cnt=0
+    for x in range (1, num/2):
+        if num % x == 0:
+            cnt += 1
+    return cnt + 1
+
+if __name__ == '__main__' :
+    num = int(input("Write a number"))
+    print(divisors(num))
         
